b83e96b422182b80de14f108cbdc794fc3de5a22
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-20 12:35:49 +0530

    colors

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+
 const String apiUrl = 'https://fakestoreapi.com/products/';
+
+const Color appPrimaryColor = Color(0xFFF3F3F3);
+const Color appSecondaryColor = Colors.white;
+const Color appAccentColor = Color(0xFF262337);
