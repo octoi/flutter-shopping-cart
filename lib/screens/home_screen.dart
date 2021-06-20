42a2115f28ff6aa4cddb0990020cf8411cd2271e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-20 12:40:12 +0530

    Heading

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +7,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: appPrimaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20.0),
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Shopping Cart",
+                style: TextStyle(fontSize: 30.0, color: appAccentColor),
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "Buy what you want",
+                style: TextStyle(fontSize: 15.0, color: appAccentColor),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
