9ad26ddf4bf87cb6a0dc8f63d1d9cb9d6866b6fb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-21 13:44:10 +0530

    constructor

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:shopping_cart/utils/constants.dart';
 
 class DetailScreen extends StatelessWidget {
+  final data;
+
+  const DetailScreen({Key? key, required this.data}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
