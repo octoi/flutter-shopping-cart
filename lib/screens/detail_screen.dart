5e09b36c603d2210ce4fb5d56ddc58740d3519a9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-21 13:43:28 +0530

    basic detail screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_cart/utils/constants.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appPrimaryColor,
+      ),
+    );
+  }
+}
