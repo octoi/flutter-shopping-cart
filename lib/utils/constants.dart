3b118755b3d56cf01646eec369d8b0c8381c0fd8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-19 13:12:22 +0530

    changed color scheme

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const String apiUrl = 'https://fakestoreapi.com/products/';
 
-const Color appPrimaryColor = Color(0xFFF3F3F3);
-const Color appSecondaryColor = Colors.white;
+const Color appPrimaryColor = Colors.white;
+const Color appSecondaryColor = Color(0xFFF3F3F3);
 const Color appAccentColor = Color(0xFF262337);
