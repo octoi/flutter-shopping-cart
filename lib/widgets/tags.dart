41e0c07ddbb692c3af194dd69b0e8c991171c410
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-19 14:51:56 +0530

    tag button

--- lib/widgets/tags.dart
+++ lib/widgets/tags.dart
@@ -14,31 +14,16 @@ class _TagsState extends State<Tags> {
     List<Widget> widgets = [];
     var categories = new Map();
 
+    widgets.add(TagButton(
+      category: 'All',
+      isSelected: true,
+    ));
+
     widget.data.forEach((item) {
       String category = item['category'];
       if (categories[category] == null) {
         categories[category] = category;
-        widgets.add(
-          Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 20.0,
-            ),
-            margin: EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(color: appAccentColor),
-            ),
-            child: Text(
-              category,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.w600,
-                color: appAccentColor,
-              ),
-            ),
-          ),
-        );
+        widgets.add(TagButton(category: category));
       }
     });
 
@@ -52,6 +37,44 @@ class _TagsState extends State<Tags> {
       physics: BouncingScrollPhysics(),
       child: Row(
         children: getCategories(),
+      ),
+    );
+  }
+}
+
+class TagButton extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+
+  const TagButton({
+    Key? key,
+    required this.category,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 20.0,
+        ),
+        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: appAccentColor),
+          color: isSelected ? appAccentColor : null,
+        ),
+        child: Text(
+          category,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+            color: !isSelected ? appAccentColor : appPrimaryColor,
+          ),
+        ),
       ),
     );
   }
